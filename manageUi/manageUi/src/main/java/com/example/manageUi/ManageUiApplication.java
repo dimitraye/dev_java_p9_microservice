5114126575171dd46aa062d4471bb4de46c47c20
dimitraye <dimitraye94@gmail.com>
2023-01-14 19:51:27 +0100

    first commit

---
+++ manageUi/manageUi/src/main/java/com/example/manageUi/ManageUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.manageUi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageUiApplication.class, args);
+	}
+
+}
