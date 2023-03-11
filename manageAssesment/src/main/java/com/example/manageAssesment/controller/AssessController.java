fd614826f48ab9a15eeb726fa0864d188e5cf611
dimitraye <dimitraye94@gmail.com>
2023-03-11 01:57:29 +0100

    microassess : commentary

--- manageAssesment/src/main/java/com/example/manageAssesment/controller/AssessController.java
+++ manageAssesment/src/main/java/com/example/manageAssesment/controller/AssessController.java
@@ -13,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+/**
+ * Manage the requests linked to an Assessment
+ */
 @Slf4j
 @RestController
 public class AssessController {
@@ -37,22 +40,35 @@ public class AssessController {
     }
 
 
+    /**
+     * Generate a report based on the patient's notes and informations
+     * @param patId
+     * @return a report
+     */
     @RequestMapping("assess/{patId}")
     public String generateReport(@PathVariable Integer patId) {
+        //uri to access a patient
         String uriPatient = baseUrl + portPatient + endpointPatient + "/" + patId;
 
+        //
         RestTemplate restTemplate = new RestTemplate();
 
         log.info("Calling endpoint get patient : " + uriPatient);
+        //
         Patient patient = restTemplate.getForObject(uriPatient, Patient.class);
 
+        //uri to access the notes of a patient
         String uriNote = baseUrl + portNote + endpointPatHistory + "?patId=" + patId;
 
         log.info("Calling endpoint get notes : " + uriNote);
+        //list of all the notes of a patient
         Note[] notes = restTemplate.getForObject(uriNote, Note[].class);
 
+        //evaluate the risk of diabetes based of the notes of a patient
         String risk = assesService.evaluateRisk(patient, List.of(notes));
+        //generate a report
         String report = assesService.generateReport(patient, risk);
+
             return report;
     }
 
--- manageAssesment/src/main/java/com/example/manageAssesment/model/Patient.java
+++ manageAssesment/src/main/java/com/example/manageAssesment/model/Patient.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+/**
+ * Model that represent the patient
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
